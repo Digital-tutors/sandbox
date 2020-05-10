fc78b7cad58f0a592eb4f415ceb7e96cadac5c95
Denis Yakovlev <denn.yakovlev@gmail.com>
2020-05-10 23:47:33 +0300

    More proper work - string removal

--- sandbox/services/operators_checker.py
+++ sandbox/services/operators_checker.py
@@ -9,8 +9,8 @@ class LanguageInfo:
             self,
             name: str,
             extension: str,
-            single_line_comment=re.compile(r"//.*?\n"),
-            multi_line_comment=re.compile(r"/\*(?:.|\n)*(?:\*/)?(?:\*/)")
+            single_line_comment=r"//.*\n?",
+            multi_line_comment=r"/\*(?:[^*]|\*(?!\/))*(?:\*/)?"
     ):
         self.name = name
         self.extension = extension
@@ -21,7 +21,7 @@ class LanguageInfo:
 class Language(Enum):
     C = LanguageInfo("c", ".c")
     CPP = LanguageInfo("cpp", ".cpp")
-    PYTHON = LanguageInfo("python", ".py", re.compile(r"#.*?\n"))
+    PYTHON = LanguageInfo("python", ".py", r"#.*\n?")
     JS = LanguageInfo("js", ".js")
     PHP = LanguageInfo("php", ".php")
     KOTLIN = LanguageInfo("kotlin", ".kt")
@@ -85,14 +85,16 @@ class OperatorsChecker:
         self.lang: Language = lang
 
     def check(self, code: str, forbidden_elements: List[LanguageElement]) -> List[LanguageElement]:
-        uncommented_code = self.__uncomment_code(code)
+        cleaned_code = self.__process_code(code)
         found_elements = []
         for element in forbidden_elements:
-            if re.search(element.get_regex(), uncommented_code) is not None:
+            if re.search(element.get_regex(), cleaned_code) is not None:
                 found_elements.append(element)
         return found_elements
 
-    def __uncomment_code(self, code: str) -> str:
+    def __process_code(self, code: str):
         single_line_comment = self.lang.value.single_line_comment
         multi_line_comment = self.lang.value.multi_line_comment
-        return re.sub(multi_line_comment, "", re.sub(single_line_comment, "", code))
+        string_regex = """(['"]).*?\\1"""
+        unified_regex = re.compile(f"{single_line_comment}|{multi_line_comment}|{string_regex}")
+        return re.sub(unified_regex, "", code)
