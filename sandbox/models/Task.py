62837b3310e60dbd8c64694ca0ba3bd602999b6c
Radmir Kashapov <radmir.kashapov.150700@gmail.com>
2020-07-07 22:17:18 +0300

    Added config package. Some works with AMQP consumer

--- sandbox/models/Task.py
+++
@@ -1,8 +0,0 @@
-class Task:
-    def __init__(self, task_id, topic_id, author_id, description, options, tests):
-        self.task_id = task_id
-        self.topic_id = topic_id
-        self.author_id = author_id
-        self.description = description
-        self.options = options
-        self.tests = tests
