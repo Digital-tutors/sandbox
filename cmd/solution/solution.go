88451eb5122b89870e1d06c991f2978963bb1a31
Denis Yakovlev <denn.yakovlev@gmail.com>
2020-09-01 18:30:50 +0300

    Operators checker implemented

--- cmd/solution/solution.go
+++ cmd/solution/solution.go
@@ -21,71 +21,71 @@ type UserID struct {
 	ID string `json:"id"`
 }
 
-
 type Solution struct {
-	SourceCode string `json:"sourceCode"`
-	Language string `json:"language"`
-	TaskID TaskID `json:"taskId"`
-	SolutionID string `json:"id"`
-	UserID UserID `json:"userId"`
-	FileName string
+	SourceCode    string `json:"sourceCode"`
+	Language      string `json:"language"`
+	TaskID        TaskID `json:"taskId"`
+	SolutionID    string `json:"id"`
+	UserID        UserID `json:"userId"`
+	FileName      string
 	DirectoryPath string
-	TimeLimit int
-	MemoryLimit int
+	TimeLimit     int
+	MemoryLimit   int
+	Constructions []string
 }
 
 type Task struct {
-	TaskID string `json:"id"`
+	TaskID  string  `json:"id"`
 	Options Options `json:"options"`
-	Tests Tests `json:"tests"`
+	Tests   Tests   `json:"tests"`
 }
 
 type Options struct {
 	Constructions []string `json:"constructions"`
-	TimeLimit string `json:"timeLimit"`
-	MemoryLimit string `json:"memoryLimit"`
+	TimeLimit     string   `json:"timeLimit"`
+	MemoryLimit   string   `json:"memoryLimit"`
 }
 
 type Tests struct {
-	Input []string `json:"input"`
+	Input  []string `json:"input"`
 	Output []string `json:"output"`
 }
 
 type LanguageConfiguration struct {
-	CodePath string `json:"code_path"`
-	LangConfigs map[string] LangConfigStructure `json:"lang_configs"`
+	CodePath    string                         `json:"code_path"`
+	LangConfigs map[string]LangConfigStructure `json:"lang_configs"`
 }
 
-type LangConfigStructure struct{
-	Name string `json:"name"`
-	SourceExtension string `json:"source_extension"`
-	IsCompilable bool `json:"is_compilable"`
-	IsNeedCompile bool `json:"is_need_compile"`
-	Compiler CompilerStructure `json:"compiler"`
-	Runner string `json:"runner"`
-	RunnerArguments string `json:"runner_args"`
-	Comments Comments `json:"comments"`
+type LangConfigStructure struct {
+	Name            string            `json:"name"`
+	SourceExtension string            `json:"source_extension"`
+	IsCompilable    bool              `json:"is_compilable"`
+	IsNeedCompile   bool              `json:"is_need_compile"`
+	Compiler        CompilerStructure `json:"compiler"`
+	Runner          string            `json:"runner"`
+	RunnerArguments string            `json:"runner_args"`
+	Comments        Comments          `json:"comments"`
 }
 
 type Comments struct {
 	SingleLineComment string `json:"single_line_comment"`
-	MultiLineComment string `json:"multi_line_comment"`
+	MultiLineComment  string `json:"multi_line_comment"`
 }
 type CompilerStructure struct {
-	Path string `json:"path"`
-	CompilerArgs string `json:"compiler_args"`
+	Path                string `json:"path"`
+	CompilerArgs        string `json:"compiler_args"`
 	ExecutableExtension string `json:"executable_extension"`
 }
 
 type Result struct {
-	ID string `json:"id"`
-	TaskID string `json:"taskId"`
-	UserID string `json:"userId"`
-	Completed bool `json:"completed"`
-	CodeReturn int `json:"codeReturn"`
+	ID         string `json:"id"`
+	TaskID     string `json:"taskId"`
+	UserID     string `json:"userId"`
+	Completed  bool   `json:"completed"`
+	CodeReturn int    `json:"codeReturn"`
 	MessageOut string `json:"messageOut"`
-	Runtime string  `json:"runtime"`
-	Memory string `json:"memory"`
+	Runtime    string `json:"runtime"`
+	Memory     string `json:"memory"`
 }
 
 func FromByteArrayToSolutionStruct(message []byte) (*Solution, error) {
@@ -102,8 +102,7 @@ func ResultToJson(result *Result) []byte {
 	return buff
 }
 
-
-func GetTaskUsingGet(url string, taskID string) (*Task,error) {
+func GetTaskUsingGet(url string, taskID string) (*Task, error) {
 	var task *Task
 
 	newUrl := strings.Replace(url, "$taskID", taskID, -1)
@@ -119,7 +118,6 @@ func GetTaskUsingGet(url string, taskID string) (*Task,error) {
 	if err != nil {
 		log.Println(err)
 	}
-
 
 	jsonError := json.Unmarshal(body, &task)
 
@@ -146,7 +144,7 @@ func UpdateSolutionInstance(solution *Solution, conf *config.Config) error {
 	solution.TimeLimit, _ = strconv.Atoi(task.Options.TimeLimit)
 	solution.FileName = solution.SolutionID
 	solution.DirectoryPath = conf.DockerSandbox.SourceFileStoragePath + solution.FileName + "/"
-
+	solution.Constructions = task.Options.Constructions
 	return err
 }
 
@@ -165,7 +163,7 @@ func SaveSolutionInFile(solution *Solution, conf *config.Config) error {
 
 	file, err := os.Create(filePath)
 
-	if err != nil{
+	if err != nil {
 		log.Println("Unable to create file:", err)
 		return err
 	}
@@ -208,9 +206,8 @@ func GetConfiguration(configurationFilePath string) (LanguageConfiguration, erro
 	return langConfig, err
 }
 
-func getExtension(configurationFilePath string, language string) (string,error) {
+func getExtension(configurationFilePath string, language string) (string, error) {
 	configuration, err := GetConfiguration(configurationFilePath)
-
 
 	return configuration.LangConfigs[language].SourceExtension, err
 }
@@ -224,13 +221,13 @@ func DeleteSolution(directoryPath string) {
 
 func NewResult(solution *Solution, completed bool, codeReturn int, messageOut string, runtime string, memory string) *Result {
 	return &Result{
-		ID: solution.SolutionID,
-		TaskID: solution.TaskID.ID,
-		UserID: solution.UserID.ID,
-		Completed: completed,
+		ID:         solution.SolutionID,
+		TaskID:     solution.TaskID.ID,
+		UserID:     solution.UserID.ID,
+		Completed:  completed,
 		CodeReturn: codeReturn,
 		MessageOut: messageOut,
-		Runtime: runtime,
-		Memory: memory,
+		Runtime:    runtime,
+		Memory:     memory,
 	}
 }
